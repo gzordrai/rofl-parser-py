26d3237741571232eb0954fd440201ae7f0479ec
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-06-09 01:01:20 +0200

    Update variable type and reposition variable for enhanced functionality and maintainability

--- src/parser/Parser.py
+++ src/parser/Parser.py
@@ -3,6 +3,7 @@ from json import loads
 from os import getcwd
 from os.path import exists, isabs, join
 from struct import unpack
+from typing import List, Tuple
 
 from Game import Game
 from exeptions.InvalidFileException import InvalidFileException
@@ -22,25 +23,24 @@ class Parser:
             raise InvalidFileException("Invalid file format. Expected a .rofl file")
 
     def parse(self) -> Game:
-        magic_numbers: bytes
-        metadata: bytes
-        file = namedtuple("length_fields", ["file_length", "metadata_offset", "metadata_length", "payload_header_offset", "payload_header_length", "payload_offset"])
-
         with open(self.file, "rb") as rofl_file:
-            magic_numbers = rofl_file.read(4)
+            magic_numbers: bytes = rofl_file.read(4)
+            FileInfo = namedtuple("FileInfo", ["file_length", "metadata_offset", "metadata_length", "payload_header_offset", "payload_header_length", "payload_offset"])
+            file_info: FileInfo
+            metadata: Tuple[List[str], List[str]]
 
             if magic_numbers != b"RIOT":
                 raise InvalidFileException("Invalid .rofl file. The file does not meet the required format.")
 
             rofl_file.seek(262)
-            file(*unpack("IIIIII", rofl_file.read(26)[2:]))
-            rofl_file.seek(file.metadata_offset)
+            file_info(*unpack("IIIIII", rofl_file.read(26)[2:]))
+            rofl_file.seek(file_info.metadata_offset)
 
-            metadata = self.parse_metadata(rofl_file.read(file.metadata_length))
+            metadata = self.parse_metadata(rofl_file.read(file_info.metadata_length))
 
         return Game()
 
-    def parse_metadata(self, data: bytes) -> None:
+    def parse_metadata(self, data: bytes) -> Tuple[List[str], List[str]]:
         metadata = loads(data.decode("utf-8"))
         players = loads(metadata["statsJson"].replace("\\", ""))
         blue_players: list[Player] = []
@@ -51,3 +51,5 @@ class Parser:
                 pass
             elif player["TEAM"] == "200":
                 pass
+
+        return blue_players, red_players
