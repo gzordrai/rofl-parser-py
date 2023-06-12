636f36fe12d06a3fc934e65bf0df8019b69764e0
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-06-12 20:46:24 +0200

    Add setup.py for library distribution setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "rofl-parser",
+    version = "1.0.0",
+    description = "",
+    packages = find_packages(),
+    classifiers = [
+        "Development Status :: 2 - Alpha",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Programming Language :: Python :: 3"
+    ]
+)
