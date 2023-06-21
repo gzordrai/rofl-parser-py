4b84a5dc6fcd0740af5858dc29a1d556e22605af
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-06-21 21:16:26 +0200

    Added a Items class to group all items data

---
+++ src/player/Items.py
@@ -0,0 +1,24 @@
+class Items:
+    def __init__(self, items: dict[str, int]) -> None:
+        self.items: dict[str, int] = items
+
+    def item_0(self) -> int:
+        return self.items.get("ITEM0", -1)
+
+    def item_1(self) -> int:
+        return self.items.get("ITEM1", -1)
+
+    def item_2(self) -> int:
+        return self.items.get("ITEM2", -1)
+
+    def item_3(self) -> int:
+        return self.items.get("ITEM3", -1)
+
+    def item_4(self) -> int:
+        return self.items.get("ITEM4", -1)
+
+    def item_5(self) -> int:
+        return self.items.get("ITEM5", -1)
+
+    def item_6(self) -> int:
+        return self.items.get("ITEM6", -1)
