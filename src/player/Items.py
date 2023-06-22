9cfc66a9179f44b58d5d714b5c73bd75a289dc22
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-06-22 21:04:31 +0200

    Added "property" decorators for class parameters

--- src/player/Items.py
+++ src/player/Items.py
@@ -2,23 +2,30 @@ class Items:
     def __init__(self, items: dict[str, int]) -> None:
         self.items: dict[str, int] = items
 
+    @property
     def item_0(self) -> int:
         return self.items.get("ITEM0", -1)
 
+    @property
     def item_1(self) -> int:
         return self.items.get("ITEM1", -1)
 
+    @property
     def item_2(self) -> int:
         return self.items.get("ITEM2", -1)
 
+    @property
     def item_3(self) -> int:
         return self.items.get("ITEM3", -1)
 
+    @property
     def item_4(self) -> int:
         return self.items.get("ITEM4", -1)
 
+    @property
     def item_5(self) -> int:
         return self.items.get("ITEM5", -1)
 
+    @property
     def item_6(self) -> int:
         return self.items.get("ITEM6", -1)
