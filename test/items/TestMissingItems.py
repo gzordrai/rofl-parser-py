683c9a586570bc36f471396ca724ecbac032652d
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-06-22 21:06:14 +0200

    Added tests for items

--- test/items/TestMissingItems.py
+++ test/items/TestMissingItems.py
@@ -8,9 +8,30 @@ path.append(root_path)
 from src.player.Items import Items
 
 
-class TestItems(TestCase):
+class TestMissingItems(TestCase):
     def setUp(self) -> None:
         self.items: Items = Items({})
+
+    def test_missing_item_0(self) -> None:
+        self.assertEqual(-1, self.items.item_0)
+
+    def test_missing_item_1(self) -> None:
+        self.assertEqual(-1, self.items.item_1)
+
+    def test_missing_item_2(self) -> None:
+        self.assertEqual(-1, self.items.item_2)
+
+    def test_missing_item_3(self) -> None:
+        self.assertEqual(-1, self.items.item_3)
+
+    def test_missing_item_4(self) -> None:
+        self.assertEqual(-1, self.items.item_4)
+
+    def test_missing_item_5(self) -> None:
+        self.assertEqual(-1, self.items.item_5)
+
+    def test_missing_item_6(self) -> None:
+        self.assertEqual(-1, self.items.item_6)
 
 if __name__ == "__main__":
     main()
