0af1e9db51fe4853aa2b560c8085f9cc9b7dda48
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-06-25 21:51:40 +0200

    Added player stats

--- src/player/Player.py
+++ src/player/Player.py
@@ -8,8 +8,18 @@ from player import Pings
 
 
 class Player:
-    def __init__(self, pings: Pings) -> None:
+    def __init__(self, uuid: str, username: str, pings: Pings) -> None:
+        self.uuid: str = uuid
+        self.username: str = username
         self.pings: Pings = pings
+
+    @property
+    def uuid(self) -> str:
+        return self.uuid
+
+    @property
+    def username(self) -> str:
+        return self.username
 
     @property
     def pings(self) -> Pings:
