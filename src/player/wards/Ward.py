653c49898bbe859cc985db8055db633fa551a635
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-06-30 22:11:15 +0200

    Added a class to manage the ward part

---
+++ src/player/wards/Ward.py
@@ -0,0 +1,28 @@
+class Wards:
+    def __init__(self) -> None:
+        pass
+
+    # TODO
+    @property
+    def killed(self) -> int:
+        return 0
+
+    # TODO
+    @property
+    def placed(self) -> int:
+        return 0
+
+    # TODO
+    @property
+    def placed_detector(self) -> int:
+        return 0
+
+    # TODO
+    @property
+    def sight_wards_bought_in_game(self) -> int:
+        return 0
+
+    # TODO
+    @property
+    def vision_wards_bought_in_game(self) -> int:
+        return 0
