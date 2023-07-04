799997dc74b250959bdf05a9f97691d0f0e53b2f
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-07-04 21:10:58 +0200

    Modification of data following the passage of metadata in the constructor

--- src/models/Game.py
+++ src/models/Game.py
@@ -9,19 +9,15 @@ class Game:
     def __init__(self, info: dict[str, str | int], players: list[Player]) -> None:
         self._info: dict[str, any] = info
         self._players: list[Player] = players
-        self.duration: int
-        self.last_game_chunk_id: int
-        self.last_key_frame_id: int
-        self.version: str
 
     def get_duration(self) -> int:
-        return self.duration
+        return self._info.get("gameLength", -1)
 
     def get_last_game_chunk_id(self) -> int:
-        return self.last_game_chunk_id
+        return self._info.get("lastGameChunkId", -1)
 
     def get_last_key_frame_id(self) -> int:
-        return self.last_key_frame_id
+        return self._info.get("lastKeyFrameId", -1)
 
     def get_version(self) -> str:
-        return self.version
+        return self._info.get("gameVersion", -1)
